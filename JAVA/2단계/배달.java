8e78e6f1c16c78f1e7eb60c2bfccc195aa2d54a3
JeongSooNa <skwjdtn3171@naver.com>
2022-03-03 16:29:41 +0900

    feat : programmers

---
+++ JAVA/2단계/배달.java
@@ -0,0 +1,21 @@
+public class 배달 {
+    class Solution {
+        public int solution(int N, int[][] road, int K) {
+            int answer = 0;
+            
+            // 제공 된 배열의 각 index값을 이용해
+            // 1번 마을부터 K거리 안에 있는 마을들의 갯수를 구해야함.
+            // N : 마을의 갯수
+            // road[][0] : 마을1
+            // road[][1] : 마을2
+            // road[][2] : 마을1,2 사이의 거리
+            // K : 배달갈 수 있는 거리 (road의 합이 K이하이면) 배달가능 마을
+            // 다이어그램, 연결그림 그려보아 확인할 것!
+
+            // 1. while문을 이용해 K이하로 도착할 수 있는지 반복해보자
+            // 2. 그 외 방법 생각
+
+            return answer;
+        }
+    }
+}
